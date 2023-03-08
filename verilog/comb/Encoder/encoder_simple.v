374ca03f4a3ed6af80753b5fc84f9fdbfd7e6bea
wilfredkisku <wilfred.kisku@gmail.com>
2023-03-08 17:39:31 +0530

    simple encoder without tb

--- verilog/comb/Encoder/encoder_simple.v
+++ verilog/comb/Encoder/encoder_simple.v
@@ -2,6 +2,16 @@ module encoder32_5(en, A, Y);
         input en;
         input [31:0] A;
         output reg [4:0] Y;
-
+	
+	integer i;
+	
+	always @(en, A) begin
+		Y = 5'b00000;
+		for (i=0;i<32;i+=1) begin
+			if (A[i] == 1) begin
+				Y = i;
+			end
+		end	
+	end
 endmodule
 
